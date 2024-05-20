ecf99912e19222aae1accedb82e0f0d2b375aaae
Damir <email>
2024-05-20 18:39:54 +0300

    Final

--- OOP2/Source.cpp
+++ OOP2/Source.cpp
@@ -6,6 +6,7 @@ int main()
 	int choice;
 	int dig1;
 	int dig2;
+
 	cout << "Enter num1:";
 	cin >> dig1;
 	cout << "Enter num2:";
@@ -13,8 +14,25 @@ int main()
 
 	cout << "1(+) or 2(-) or 3(*) or 4(/):" << endl;
 	cin >> choice;
+
 	if (choice == 1)
 	{
-		cout << dig1 << " + " << dig2 << " = " << dig1+dig2;
+		cout << dig1 << " + " << dig2 << " = " << dig1 + dig2;
+	}
+	else if (choice == 2)
+	{
+		cout << dig1 << " - " << dig2 << " = " << dig1 - dig2;
+	}
+	else if (choice == 3)
+	{
+		cout << dig1 << " * " << dig2 << " = " << dig1 * dig2;
+	}
+	else if (choice == 4)
+	{
+		cout << dig1 << " / " << dig2 << " = " << dig1 / dig2;
+	}
+	else
+	{
+		cout << "Wrong number:(";
 	}
 }
