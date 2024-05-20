a7d7b3a3838600099a48d1fb78a924c397b2a01b
Damir <email>
2024-05-20 18:20:15 +0300

    Calculator

--- OOP2/Source.cpp
+++ OOP2/Source.cpp
@@ -3,5 +3,18 @@ using namespace std;
 
 int main()
 {
+	int choice;
+	int dig1;
+	int dig2;
+	cout << "Enter num1:";
+	cin >> dig1;
+	cout << "Enter num2:";
+	cin >> dig2;
 
+	cout << "1(+) or 2(-) or 3(*) or 4(/):" << endl;
+	cin >> choice;
+	if (choice == 1)
+	{
+		cout << dig1 << " + " << dig2 << " = " << dig1+dig2;
+	}
 }
